160bd411f53dc625b39dd203ccbdb2f9e6043875
Andrei <nzpopa@users.noreply.github.com>
2021-11-14 15:58:43 +0200

    Update kadena-config.js

--- templates/common/kadena-config.js
+++ templates/common/kadena-config.js
@@ -54,7 +54,7 @@ var kadenaAPI = {
     host: host,
     creationTime: creationTime,
     //gas price at lowest possible denomination
-    gasPrice: 0.00000000001,
+    gasPrice: 0.000000001,
     //high gas limit for tx
     gasLimit: 10000,
     //time a tx lives in mempool since creationTime
